d72f911c10161cafdf92e297f68501d6c7ee274f
SergeiZZ <you@example.com>
2020-11-23 23:53:17 +0300

    upd

---
+++ build/precache-manifest.cb4c61afb18deba29a92d8b024ed316b.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52ff1770b96a00c31d2e0c541422b8f4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9bbf4e0b6dd708afff23",
+    "url": "/static/css/2.ecc15aba.chunk.css"
+  },
+  {
+    "revision": "e5ec3d8b06053f079b63",
+    "url": "/static/css/main.219ecfcb.chunk.css"
+  },
+  {
+    "revision": "9bbf4e0b6dd708afff23",
+    "url": "/static/js/2.2dbcb925.chunk.js"
+  },
+  {
+    "revision": "b2eb81eebe9d6c30dce85716b8bb935a",
+    "url": "/static/js/2.2dbcb925.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e5ec3d8b06053f079b63",
+    "url": "/static/js/main.51b585f7.chunk.js"
+  },
+  {
+    "revision": "60e5a5922bb048142705",
+    "url": "/static/js/runtime-main.e3cd0869.js"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "c641dbee1d75892e4d88bdc31560c91b",
+    "url": "/static/media/Montserrat-SemiBold.c641dbee.ttf"
+  },
+  {
+    "revision": "15998c9a83f9e867467d9a9dd4d6e24d",
+    "url": "/static/media/fall-chart.15998c9a.svg"
+  },
+  {
+    "revision": "4a69675d840c270d26bc775cb1fd7037",
+    "url": "/static/media/logout.4a69675d.svg"
+  },
+  {
+    "revision": "27fd11e7fafba3ea03df7baaadd1391d",
+    "url": "/static/media/off.27fd11e7.svg"
+  },
+  {
+    "revision": "2e6ec85a6223f2354a1f0c63005a2a5d",
+    "url": "/static/media/on.2e6ec85a.svg"
+  },
+  {
+    "revision": "28ddfaeac5c797f6c038710acef3c8a7",
+    "url": "/static/media/rise-chart.28ddfaea.svg"
+  },
+  {
+    "revision": "bbd2ebb315aa393b6167043b8b654471",
+    "url": "/static/media/scr1.bbd2ebb3.png"
+  },
+  {
+    "revision": "1a55ec4c3198e2b33f5a09f22ba1e261",
+    "url": "/static/media/scr2.1a55ec4c.png"
+  },
+  {
+    "revision": "e403fd95a444f66e73fe5ace225bc3f8",
+    "url": "/static/media/scr3.e403fd95.png"
+  },
+  {
+    "revision": "9061fd8d4363b7fe5c865d713d1a0341",
+    "url": "/static/media/scr4.9061fd8d.png"
+  }
+]);
